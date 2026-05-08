e0448c00421ab6b1dcc68b619cc456d205f49290
jbs-jyj <jbs36_1405@js.hs.kr>
2026-05-08 19:12:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-body {
-  font-family: Arial;
-  background: #f4f4f4;
-  padding: 40px;
-}
+async function searchBooks() {
 
-.container {
-  max-width: 800px;
-  margin: auto;
-}
+  const keyword =
+    document.getElementById("keyword").value;
 
-input {
-  padding: 12px;
-  width: 300px;
-}
+  const url =
+    `https://openapi.seoul.go.kr:8088/` +
+    `ad45fb2710a84c1d182b19ee083b656290d8d385860bd75c7c1ac35d83ad195c/` +
+    `xml/SearchCatalogService/1/5/${keyword}`;
 
-button {
-  padding: 12px;
-  cursor: pointer;
-}
+  try {
 
-.book {
-  background: white;
-  margin-top: 20px;
-  padding: 20px;
-  border-radius: 10px;
+    const response = await fetch(url);
+
+    const text = await response.text();
+
+    const parser = new DOMParser();
+
+    const xml =
+      parser.parseFromString(text, "text/xml");
+
+    const rows =
+      xml.getElementsByTagName("row");
+
+    const result =
+      document.getElementById("result");
+
+    result.innerHTML = "";
+
+    for (let row of rows) {
+
+      const title =
+        row.getElementsByTagName("TITLE")[0]?.textContent;
+
+      const author =
+        row.getElementsByTagName("AUTHOR")[0]?.textContent;
+
+      const publisher =
+        row.getElementsByTagName("PUBLISHER")[0]?.textContent;
+
+      result.innerHTML += `
+        <div class="book">
+          <h3>${title}</h3>
+          <p>저자: ${author}</p>
+          <p>출판사: ${publisher}</p>
+        </div>
+      `;
+    }
+
+  } catch(error) {
+
+    console.error(error);
+
+    alert("오류 발생");
+  }
 }
